836fe2f08b7f97158f73753f6433c54d50002f12
Trevor Herrera <therrera39@cnm.edu>
2025-10-15 16:57:11 -0600

    Implement navigation component and update routing structure
    # Conflicts:
    #	frontend/app/components/navigation.tsx
    #	frontend/app/layouts/root-layout.tsx

--- frontend/app/components/navigation.tsx
+++ frontend/app/components/navigation.tsx
@@ -1,20 +1,23 @@
+
 import {Navbar, NavbarBrand, NavbarToggle, NavbarCollapse, NavbarLink} from "flowbite-react";
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import {Link} from "react-router";
 
-export default function Navigation() {
-  return (
-    <Navbar>
-      <NavbarBrand href="/">
-
-      </NavbarBrand>
-      <NavbarToggle/>
-      <NavbarCollapse>
-        <NavbarLink href="/about">
-          About
-        </NavbarLink>
-        <NavbarLink href="/pizza">
-          Pizza
-        </NavbarLink>
-      </NavbarCollapse>
-    </Navbar>
-  )
+export function Navigation() {
+    return (
+        <Navbar fluid rounded className={"flex flex-row-reverse"}>
+            <NavbarBrand as={Link} href="https://flowbite-react.com">
+                <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+            </NavbarBrand>
+            <NavbarToggle/>
+            <NavbarCollapse>
+                <NavbarLink href="#" active>Home</NavbarLink>
+                <NavbarLink as={Link} href="#">About</NavbarLink>
+                <NavbarLink href="#">Services</NavbarLink>
+                <NavbarLink href="#">Pricing</NavbarLink>
+                <NavbarLink href="#">Expenses</NavbarLink>
+            </NavbarCollapse>
+        </Navbar>
+    );
 }
