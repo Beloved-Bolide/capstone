efb0d9fb64508b9fc3bd9c80cfe1c2720ff33015
Trevor Herrera <therrera39@cnm.edu>
2025-10-15 16:57:11 -0600

    Implement navigation component and update routing structure

--- frontend/app/layouts/root-layout.tsx
+++ frontend/app/layouts/root-layout.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from "react-router";
+import {Navigation} from "~/components/navigation";
 
 export default function RootLayout() {
 	return (
 		<>
+            <Navigation/>
 		    <Outlet />
 	    </>
 	)
