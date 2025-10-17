faa050f4acc744bfac15dbe5858e8a24afeee724
Trevor Herrera <therrera39@cnm.edu>
2025-10-17 10:37:54 -0600

    Refactor root layout formatting for improved readability

--- frontend/app/layouts/root-layout.tsx
+++ frontend/app/layouts/root-layout.tsx
@@ -2,10 +2,10 @@ import {Outlet} from "react-router";
 import {Navigation} from "~/components/navigation";
 
 export default function RootLayout() {
-	return (
-		<>
-            <Navigation/>
-		    <Outlet />
-	    </>
-    )
+  return (
+    <>
+      <Navigation/>
+      <Outlet />
+	</>
+  )
 }
