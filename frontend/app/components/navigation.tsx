a0a2e6607ad545964cf3da9de6ecb6a9b575fc5a
Trevor Herrera <therrera39@cnm.edu>
2025-10-17 12:02:34 -0600

    Changed home.tsx and navigation.tsx

--- frontend/app/components/navigation.tsx
+++ frontend/app/components/navigation.tsx
@@ -5,21 +5,23 @@ import { Link } from "react-router";
 export function Navigation() {
   return (
     <Navbar fluid rounded>
-      <NavbarBrand as={Link} href="https://flowbite-react.com">
+      <NavbarBrand as={ Link } href="https://flowbite-react.com">
         <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite React
+        </span>
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink href="#" active>
+        <NavbarLink href="#" to="/" active>
           Home
         </NavbarLink>
-        <NavbarLink as={Link} href="#">
-          About
+        <NavbarLink as={ Link } to="/dashboard">
+          Dashboard
         </NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Pricing</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
+        <NavbarLink href="#">
+          Services
+        </NavbarLink>
       </NavbarCollapse>
     </Navbar>
   );
