aa96527d139913c36ce73eaacf3a954d3dd5e2f6
Trevor Herrera <therrera39@cnm.edu>
2025-10-17 08:47:48 -0600

    Updated navbar

--- frontend/app/components/navigation.tsx
+++ frontend/app/components/navigation.tsx
@@ -4,12 +4,12 @@ import {Link} from "react-router";
 
 export function Navigation() {
     return (
-        <Navbar fluid rounded className={"flex flex-row-reverse"}>
+        <Navbar fluid rounded className={"flex items-center gap-2"}>
+            <NavbarToggle className="md:!inline-flex" />
             <NavbarBrand as={Link} href="https://flowbite-react.com">
                 <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
             </NavbarBrand>
-            <NavbarToggle/>
             <NavbarCollapse>
                 <NavbarLink href="#" active>Home</NavbarLink>
                 <NavbarLink as={Link} href="#">About</NavbarLink>
