36e39979ec19bd515ff2f66d2b1dc1926d30101a
Blu Xenethos <blu.xenethos@gmail.com>
2025-10-24 12:12:07 -0600

    implement navbar

--- frontend/app/layouts/root-layout.tsx
+++ frontend/app/layouts/root-layout.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from "react-router";
+import {Navbar} from "flowbite-react";
 
 export default function RootLayout() {
   return (
     <>
+      <Navbar/>
       <Outlet/>
     </>
   )
