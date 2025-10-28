2076cb6bcfe43eabedc49a5b5f878ecadbc944b8
Trevor Herrera <therrera39@cnm.edu>
2025-10-28 13:12:27 -0600

    Refactor SQL script: standardize table formatting, align column definitions, and enhance readability

--- sql/filewise.sql
+++ sql/filewise.sql
@@ -1,4 +1,3 @@
-
 DROP TABLE IF EXISTS file;
 DROP TABLE IF EXISTS record;
 DROP TABLE IF EXISTS category;
@@ -6,68 +5,65 @@ DROP TABLE IF EXISTS folder;
 DROP TABLE IF EXISTS "user";
 
 CREATE TABLE IF NOT EXISTS "user" (
-                        id UUID PRIMARY KEY,
-                        activation_token CHAR(32),
-                        email VARCHAR(128) UNIQUE NOT NULL,
-                        email_notifications boolean DEFAULT true,
-                        name VARCHAR(64) NOT NULL,
-                        password_hash CHAR(97) NOT NULL
+    id                  UUID PRIMARY KEY,
+    activation_token    CHAR(32),
+    email               VARCHAR(128) UNIQUE NOT NULL,
+    email_notifications boolean DEFAULT true,
+    name                VARCHAR(64)         NOT NULL,
+    password_hash       CHAR(97)            NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS folder (
-                          id UUID PRIMARY KEY,
-                          user_id UUID,
-                          name VARCHAR(64) NOT NULL,
-                          parent_folder_id UUID,
-                          FOREIGN KEY (user_id) REFERENCES "user"(id),
-                          FOREIGN KEY (parent_folder_id) REFERENCES folder(id)
+    id               UUID PRIMARY KEY,
+    parent_folder_id UUID,
+    user_id          UUID,
+    name             VARCHAR(64) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id),
+    FOREIGN KEY (parent_folder_id) REFERENCES folder (id)
 );
-CREATE INDEX ON folder(user_id);
-CREATE INDEX ON folder(parent_folder_id);
+CREATE INDEX ON folder (user_id);
+CREATE INDEX ON folder (parent_folder_id);
 
 CREATE TABLE IF NOT EXISTS category (
-                          id  UUID PRIMARY KEY NOT NULL,
-                          color VARCHAR(32),
-                          icon VARCHAR(128),
-                          name VARCHAR(32) UNIQUE NOT NULL
+    id    UUID PRIMARY KEY NOT NULL,
+    color VARCHAR(32),
+    icon  VARCHAR(128),
+    name  VARCHAR(32) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS record (
-                          id UUID PRIMARY KEY,
-                          folder_id UUID NOT NULL,
-                          category_id UUID NOT NULL,
-                          amount DECIMAL(10,2),
-                          company_name VARCHAR(64),
-                          coupon_code VARCHAR(32),
-                          description VARCHAR(512),
-                          exp_date DATE,
-                          last_accessed_at TIMESTAMPTZ,
-                          notify_on BOOLEAN DEFAULT FALSE,
-                          product_id VARCHAR(32),
-                          purchase_date DATE,
-                          warranty_exp DATE,
-
-                          FOREIGN KEY (folder_id) REFERENCES folder(id),
-                          FOREIGN KEY (category_id) REFERENCES category(id)
+    id               UUID PRIMARY KEY,
+    folder_id        UUID,
+    category_id      UUID,
+    amount           DECIMAL(10, 2),
+    company_name     VARCHAR(64),
+    coupon_code      VARCHAR(32),
+    description      VARCHAR(512),
+    exp_date         DATE,
+    last_accessed_at TIMESTAMPTZ,
+    notify_on        BOOLEAN DEFAULT FALSE,
+    product_id       VARCHAR(32),
+    purchase_date    DATE,
+    warranty_exp     DATE,
+    FOREIGN KEY (folder_id) REFERENCES folder (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
-CREATE INDEX ON record(folder_id);
-CREATE INDEX ON record(category_id);
+CREATE INDEX ON record (folder_id);
+CREATE INDEX ON record (category_id);
 
 CREATE TABLE IF NOT EXISTS file (
-                        id UUID PRIMARY KEY,
-                        record_id UUID NOT NULL,
-                        description VARCHAR(256),
-                        document_date DATE,
-                        file_key VARCHAR(32),
-                        file_url VARCHAR(256) NOT NULL,
-                        is_starred BOOLEAN DEFAULT FALSE,
-                        name VARCHAR(32) NOT NULL,
-                        ocr_data TEXT,
-
-                        FOREIGN KEY(record_id) REFERENCES record(id)
-
+    id            UUID PRIMARY KEY,
+    record_id     UUID,
+    description   VARCHAR(256),
+    document_date DATE,
+    file_key      VARCHAR(32),
+    file_url      VARCHAR(256) NOT NULL,
+    is_starred    BOOLEAN DEFAULT FALSE,
+    name          VARCHAR(32)  NOT NULL,
+    ocr_data      TEXT,
+    FOREIGN KEY (record_id) REFERENCES record (id)
 );
-CREATE INDEX ON file(record_id);
+CREATE INDEX ON file (record_id);
 
 
 
