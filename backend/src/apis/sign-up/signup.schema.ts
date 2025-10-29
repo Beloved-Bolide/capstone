83af41b7f8b7686e219ee0b395084799facf0868
Denise Rose <230087781+dmascardo@users.noreply.github.com>
2025-10-29 08:31:44 -0600

    Create signup.schema.ts

---
+++ backend/src/apis/sign-up/signup.schema.ts
@@ -0,0 +1,17 @@
+import {z} from "zod/v4";
+import {PrivateUserSchema} from "../user/user.model.ts";
+
+export const SignUpUserSchema = PrivateUserSchema
+.omit({hash: true, activationToken: true, })
+.extend({
+  passwordConfirm: z.string('password confirmation is required')
+  .min(8, 'Password cannot be less than 8 characters long')
+  .max(32, 'profile password '),
+
+  password: z.string('password confirmation is required')
+  .min(8, 'profile password cannot be less than 8 characters long')
+  .max(32, 'profile password cannot be over than 32 characters long')
+})
+.refine(data=> data.password === data.passwordConfirm, {
+  message:'passwords do not match'
+})
