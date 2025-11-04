845c3606f226fe57f258308c42650d90d5965b22
Trevor Herrera <therrera39@cnm.edu>
2025-11-04 13:46:08 -0700

    Refactored getCategoryByCategoryIdController: improved response formatting, enhanced error handling, updated variable spacing, and clarified success message for better readability.

--- backend/src/apis/category/category.controller.ts
+++ backend/src/apis/category/category.controller.ts
@@ -13,30 +13,34 @@ import { selectCategoryByCategoryId } from './category.model.ts'
  * @param response from the server to the client with all Categories by CategoryID or an error message
  * @return  A promise containing the response for the client with the requested information,
  * or null if the information could not be found, set to the data field. **/
-export async function getCategoryByCategoryIdController(request: Request, response: Response) : Promise<void> {
-  try{
+export async function getCategoryByCategoryIdController (request: Request, response: Response) : Promise<void> {
+  try {
 
     //validate the id coming from the request parameters
     const validationResult = CategorySchema.pick({id: true}).safeParse(request.params)
 
     // if the validation is unsuccessful, return a preformatted response to the client
-
-    if(!validationResult.success){
+    if (!validationResult.success) {
       zodErrorResponse(response, validationResult.error)
       return
     }
 
     //grab the id off of the validated request parameters
-    const {id} =validationResult.data
+    const { id } =validationResult.data
 
     //grab the Category by id
     const data = await selectCategoryByCategoryId(id)
 
     // return the response to the client with the requested information
-    response.json({status: 200, message: null, data})
+    response.json({
+      status: 200,
+      data,
+      message: 'Successfully retrieved category by id'
+    })
+
   }catch (error: unknown){
-    console.error(error)
     // if an error occurs, return a preformatted response to the client
+    console.error(error)
     serverErrorResponse(response, null)
   }
 }
