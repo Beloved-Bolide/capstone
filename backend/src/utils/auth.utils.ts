dd1951179ff9720c77fc2ea8702727da50ce535d
Trevor Herrera <therrera39@cnm.edu>
2025-11-04 19:42:19 -0700

    Standardized import formatting, function signature spacing, and improved comments in response.utils.ts and auth.utils.ts for consistency.

--- backend/src/utils/auth.utils.ts
+++ backend/src/utils/auth.utils.ts
@@ -1,29 +1,30 @@
 import * as argon2 from 'argon2'
 import * as crypto from 'crypto'
 import pkg from 'jsonwebtoken'
-const {sign} = pkg
+const { sign } = pkg
 
 // generate a jwt token
-export function generateJwt(payload: object, signature: string): string {
+export function generateJwt (payload: object, signature: string): string {
   const setExpInSecondsSinceEpoch = (currentTimestamp: number): number => {
     const oneHourInMilliseconds: number = 3600000 * 3
     const futureTimestamp: number = Math.round(currentTimestamp) + oneHourInMilliseconds
     const futureTimestampInSeconds: number = futureTimestamp / 1000
     return Math.round(futureTimestampInSeconds)
   }
+
   const iat = new Date().getTime()
   const exp = setExpInSecondsSinceEpoch(iat)
   return sign({exp, auth: payload, iat}, signature)
 }
 
 // generate a random activation token
-export function setActivationToken(): string {
+export function setActivationToken (): string {
   return crypto.randomBytes(16).toString('hex')
 }
 
 // hash a password
-export async function setHash(password: string): Promise<string> {
-  return await argon2.hash(
+export async function setHash (password: string): Promise<string> {
+  return await argon2.hash (
     password,
     {
       type: argon2.argon2id,
@@ -34,7 +35,7 @@ export async function setHash(password: string): Promise<string> {
 
 // validate a password
 export async function validatePassword(hash: string, password: string): Promise<boolean> {
-  return await argon2.verify(
+  return await argon2.verify (
     hash,
     password
   )
