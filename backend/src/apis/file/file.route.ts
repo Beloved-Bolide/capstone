0af8c0eb2f25e490fe5291cad77a228ffe691ff9
Blu Xenethos <blu.xenethos@gmail.com>
2025-11-09 20:04:47 -0700

    file controllers

---
+++ backend/src/apis/file/file.route.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+import {
+  getFileByFileIdController,
+  getFileByRecordIdController,
+  postFileController,
+  updateFileController
+} from './file.controller.ts'
+import { isLoggedInController } from '../../utils/controllers/is-logged-in.controller.ts'
+
+
+const basePath = '/apis/file' as const
+const router = Router()
+
+router.route('/')
+.post(isLoggedInController, postFileController)
+
+router.route('/id/:id')
+.get(isLoggedInController, getFileByFileIdController)
+.put(isLoggedInController, updateFileController)
+
+router.route('/record/:id')
+.get(isLoggedInController, getFileByRecordIdController)
+
+export const fileRoute = { basePath, router }
