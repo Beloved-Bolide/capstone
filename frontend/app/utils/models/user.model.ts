6d0297bf09ade3877c2dab9db94423edb933c6b5
Jijo Jose <jjose6@cnm.edu>
2025-11-12 10:52:35 -0700

    added frontend utils-models-user.modelfile

---
+++ frontend/app/utils/models/user.model.ts
@@ -0,0 +1,15 @@
+import {z} from "zod";
+
+
+export const UserSchema = z.object({
+  id: z.uuidv7('Please provide a valid uuid for id'),
+  email: z.email('Please provide a valid email')
+  .max(128, 'please provide a valid email (max 128 characters)'),
+  notifications: z.boolean('please provide a valid notifications (true or false)'),
+  name: z.string('Please provide a valid name')
+  .trim()
+  .min(1, 'please provide a valid name(min 1 characters)')
+  .max(32, 'please provide a valid name(max 32 characters)'),
+})
+
+export type User =  z.infer<typeof UserSchema>
