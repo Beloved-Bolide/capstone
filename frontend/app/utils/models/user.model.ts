3c2e3a1133eba535ee52c4f19ad24801f270ff53
Jijo Jose <jjose6@cnm.edu>
2025-11-13 09:36:46 -0700

    Sign-up successfully working

--- frontend/app/utils/models/user.model.ts
+++ frontend/app/utils/models/user.model.ts
@@ -11,7 +11,11 @@ export const UserSchema = z.object({
   email: z.email('Please provide a valid email')
   .max(128, 'please provide a valid email (max 128 characters)'),
   notifications: z.boolean('please provide a valid notifications (true or false)')
-  .parse (true)
+  .parse (true),
+  name: z.string('Please provide a valid name')
+  .trim()
+  .min(1, 'please provide a valid name(min 1 characters)')
+  .max(32, 'please provide a valid name(max 32 characters)'),
 })
 
 export type User =  z.infer<typeof UserSchema>
@@ -24,6 +28,7 @@ export const SignUpSchema = UserSchema.omit({id:true, notifications: true})
   password: z.string('password is required')
   .min(8, 'profile password cannot be less than 8 characters')
   .max(32,'profile password cannot be over 32 characters')
+
 })
 .refine(data => data.password === data.passwordConfirm, {
   message: 'passwords do not match',
