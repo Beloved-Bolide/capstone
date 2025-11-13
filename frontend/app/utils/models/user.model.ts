84a558e58d5e1b66bd22b32e817f2c55122d11d2
Trevor Herrera <therrera39@cnm.edu>
2025-11-13 15:00:11 -0700

    Updated  to include  and fixed formatting in

--- frontend/app/utils/models/user.model.ts
+++ frontend/app/utils/models/user.model.ts
@@ -4,12 +4,12 @@ import { z } from 'zod/v4'
 export const UserSchema = z.object({
   id: z.uuidv7('Please provide a valid uuid for id'),
   email: z.email('Please provide a valid email')
-    .max(128, 'please provide a valid email (max 128 characters)'),
+    .max(128, 'Please provide a valid email (max 128 characters)'),
   name: z.string('Please provide a valid name')
     .trim()
-    .min(1, 'please provide a valid name(min 1 characters)')
-    .max(32, 'please provide a valid name(max 32 characters)'),
-  notifications: z.boolean('please provide a valid notifications (true or false)')
+    .min(1, 'Please provide a valid name (min 1 characters)')
+    .max(32, 'Please provide a valid name (max 32 characters)'),
+  notifications: z.boolean('Please provide a valid notifications (true or false)')
 })
 
 export type User = z.infer<typeof UserSchema>
