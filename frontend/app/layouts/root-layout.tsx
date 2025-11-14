823969b7932709b24fc01802d5933d78044d35ad
Trevor Herrera <therrera39@cnm.edu>
2025-11-13 17:30:55 -0700

    m

--- frontend/app/layouts/root-layout.tsx
+++ frontend/app/layouts/root-layout.tsx
@@ -1,16 +1,18 @@
-import {Outlet} from 'react-router'
-import {Navbar} from '~/components/navbar'
+import { Outlet } from 'react-router'
+import { Navbar } from '~/components/navbar'
 
-export default function RootLayout() {
+
+export default function RootLayout () {
+
   const handleMenuClick = () => {
     // Handle mobile menu toggle
     console.log('Menu clicked')
   }
 
   return (
-  <>
-    {/*<Navbar onMenuClick={handleMenuClick}/>*/}
-    <Outlet/>
-  </>
+    <>
+      {/*<Navbar onMenuClick={handleMenuClick}/>*/}
+      <Outlet/>
+    </>
   )
 }
